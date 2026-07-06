e31ce76f74e4dfeea4f378ecee8d78d163d39616
ayjay-9 <ayobanjoemmanuel1@gmail.com>
2026-07-06 13:59:16 +0100

    add placeholder test

--- experiments/tests.py
+++ experiments/tests.py
@@ -1,3 +1,7 @@
+from django.apps import apps
 from django.test import TestCase
 
-# Create your tests here.
+
+class ExperimentsAppConfigTests(TestCase):
+    def test_app_is_installed(self):
+        self.assertTrue(apps.is_installed("experiments"))
